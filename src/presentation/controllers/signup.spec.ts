a9d15bd9f2e6fe1a8d801a9506b90cfe776ebb40
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-02-14 21:22:22 -0300

    test: ensure SignUpController returns 400 if no name is provider

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('Signup Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
