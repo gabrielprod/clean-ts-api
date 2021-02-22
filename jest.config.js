e1228d382229bed08b4c1b209b373d5d5962ce07
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-02-21 21:10:15 -0300

    Revert "chore: add jest configs for mongodb"
    This reverts commit bf118e691c180cf9acb08f49aaeac2c49b726e79.

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
