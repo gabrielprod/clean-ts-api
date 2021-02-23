31f308d64e4b44ca372a3d62fc58cd7731aa0278
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-02-23 17:25:20 -0300

    refactor: move sut creation to a factory help method

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,6 +1,10 @@
 import { mongoHelper } from '../helper/mongo-helper'
 import { AccountMongoRepository } from './account'
 
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await mongoHelper.connect(process.env.MONGO_URL)
@@ -11,7 +15,7 @@ describe('Account Mongo Repository', () => {
   })
 
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
@@ -22,6 +26,6 @@ describe('Account Mongo Repository', () => {
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
-    expect(account.name).toBe('any_password')
+    expect(account.password).toBe('any_password')
   })
 })
