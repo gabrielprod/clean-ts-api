de0edfb386e44686f3913a627a3855410e6781a7
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-03-02 00:18:58 -0300

    feat: ensure LogController returns the same result of the controller

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -8,7 +8,7 @@ export class LogControllerDecorator implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.controller.handle(httpRequest)
-    return null
+    const dataRegistered = await this.controller.handle(httpRequest)
+    return dataRegistered
   }
 }
