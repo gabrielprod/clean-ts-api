85ec28692a42e125069223731603694f1326a8af
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-03-05 23:47:54 -0300

    test: add LogMongoRepositoryto SignUpFactory

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -16,7 +16,7 @@ const makeController = (): Controller => {
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log (stack: string): Promise<void> {
+    async logError (stack: string): Promise<void> {
       return new Promise(resolve => resolve())
     }
   }
@@ -85,7 +85,7 @@ describe('', () => {
   test('Should call LogErrorRepository with correct error if controller returns a ServerError', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
 
-    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError')
     jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => resolve(makeFakeServerError())))
 
     await sut.handle(makeHttpRequest())
