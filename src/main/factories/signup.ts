39797ee339678e7f1ebbed1c98b57311a5deb9e3
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-05 14:14:51 -0300

    refactor: move required field validation to the ValidationComposity

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -6,13 +6,14 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(12)
   const accountMongoRepo = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepo)
   const emailValidatorAdapter = new EmailValidatorAdapter()
-  const signupController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signupController = new SignUpController(emailValidatorAdapter, dbAddAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signupController, logMongoRepository)
 }
---
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -0,0 +1,18 @@
+import { Validation } from './validation'
+
+export class ValidationComposite implements Validation {
+  private readonly validations: Validation[]
+
+  constructor (validations: Validation[]) {
+    this.validations = validations
+  }
+
+  validate (input: any): Error {
+    for (const validation of this.validations) {
+      const err = validation.validate(input)
+      if (err) {
+        return err
+      }
+    }
+  }
+}
