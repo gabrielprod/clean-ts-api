019356fd842019cd41656a3fc9709ea0c8ee3b3a
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-06 23:03:11 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- src/presentation/helpers/validators/required-filed-validation.spec.ts
+++ src/presentation/helpers/validators/required-filed-validation.spec.ts
@@ -7,4 +7,10 @@ describe('Required Field Validation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should not return if validations succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
 })
