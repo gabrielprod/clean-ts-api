58b931c3a21c5fcfbfc798295127029540803fc8
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-09 15:10:14 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

--- src/presentation/helpers/validators/validation-composite.ts
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -3,6 +3,7 @@ import { Validation } from '../../protocols/validation'
 export class ValidationComposite implements Validation {
   private readonly validations: Validation[]
 
+  // irá ser feita todas as validações a partir desse componente
   constructor (validations: Validation[]) {
     this.validations = validations
   }
