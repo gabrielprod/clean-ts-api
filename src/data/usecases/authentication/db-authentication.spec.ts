63e2a0221501ce240836f5b5295161cd6cdbd3ed
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-10 18:04:47 -0300

    refactor: move DbAuthentication protocols to a single file

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,10 +1,11 @@
+import { DbAthentication } from './db-authentication'
 import { AccountModel } from '../../../domain/models/account'
 import { AuthenticationModel } from '../../../domain/usecases/authentication'
 import { HashComparer } from '../../protocols/criptography/hash-comparer'
 import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
-import { DbAthentication } from './db-authentication'
+
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
