dbb56f937243cd370655b103f21dc0b993b221ff
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-12 22:10:24 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,14 @@
+import { Encrypter } from '../../../data/protocols/criptography/encrypter'
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter implements Encrypter {
+  private readonly secret: string
+  constructor (secret: string) {
+    this.secret = secret
+  }
+
+  async encrypt (value: string): Promise<string> {
+    await jwt.sign({ id: 'any_id' }, this.secret)
+    return null
+  }
+}
