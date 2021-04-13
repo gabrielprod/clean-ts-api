4c6afbb68396fd54b4ea658ab30058867d008679
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-13 17:39:25 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -10,4 +10,12 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountD = result.ops[0]
     return mongoHelper.map(accountD)
   }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await mongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    if (account) {
+      return mongoHelper.map(account)
+    }
+  }
 }
