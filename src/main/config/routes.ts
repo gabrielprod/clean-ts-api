2a883e48ad96c9a6849e62527e5f9fb7a6454319
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-18 21:56:23 -0300

    refactor: fixing the errors path

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,12 @@
 import { Express, Router } from 'express'
+import path from 'path'
 import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   // fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
-  readdirSync(`${__dirname} /../routes`).map(async file => {
+  readdirSync(path.join(__dirname, '/../routes')).map(async file => {
     if (!file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
