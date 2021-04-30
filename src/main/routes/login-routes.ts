5aa59d37c924047e5774c016f280a878b354246a
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-30 11:07:08 -0300

    refactor: separate factories in folders

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/signup/signup-factory'
-import { makeLoginController } from '../factories/login/login-factory'
+import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
+import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
+import { makeLogControllerDecorator } from '../factories/decorators/log-controller-decarator'
 
 export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeSignUpController()))
-  router.post('/login', adaptRoute(makeLoginController()))
+  router.post('/signup', adaptRoute(makeLogControllerDecorator(makeSignUpController())))
+  router.post('/login', adaptRoute(makeLogControllerDecorator(makeLoginController())))
 }
