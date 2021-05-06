6014ca99d535543ab269e7f27dab83723899c877
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-05-06 01:42:05 -0300

    feat: ensureAddSurveyController returns a badRequest error if validation fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '../../../helpers/http/http-helper'
 import { Validation } from '../../../protocols'
 import { Controller, HttpRequest, HttpResponse } from './add-survey-controller-protocols'
 
@@ -9,7 +10,12 @@ export class AddSurveyController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+
+    if (error) {
+      return badRequest(error)
+    }
+
     return new Promise(resolve => resolve(null))
   }
 }
