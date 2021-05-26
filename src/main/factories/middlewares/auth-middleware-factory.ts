df8a8a8758f763ff15e9e6a101fb4474032a7128
gabriel <gabriel.pr07@hotmail.com>
2021-05-26 18:48:53 -0300

    feat: ensure add survey route returns 403 if no accessToken is provided

---
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -0,0 +1,8 @@
+import { AuthMiddleware } from "../../../presentation/middlewares/auth-middleware"
+import { Middleware } from "../../../presentation/protocols"
+import { makeDbLoadAccountByToken } from "../usecases/account/load-account-by-token/db-load-account-by-token-factory"
+
+
+export const makeAuthMiddleware = (role?: string): Middleware => {
+  return new AuthMiddleware(makeDbLoadAccountByToken(), role)
+}
