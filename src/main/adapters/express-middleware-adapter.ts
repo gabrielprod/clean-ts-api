a901963de04030ebec2d5650e2e5052b1a43c164
gabriel <gabriel.pr07@hotmail.com>
2021-05-27 21:29:33 -0300

    test: ensure add survey route return 204 if valid  accessToken is provided

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -10,7 +10,7 @@ export const adaptMiddleware = (middleware: Middleware): RequestHandler => {
     const httpResponse = await middleware.handle(httpRequest)
 
     if (httpResponse.statusCode == 200) {
-      Object.assign(req, httpResponse)
+      Object.assign(req, httpResponse.body)
       next()
     } else {
       res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,12 @@ import request from 'supertest'
 import app from '../config/app'
 import { mongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -17,6 +21,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await mongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await mongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -33,6 +39,41 @@ describe('Survey Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+
+      const res = await accountCollection.insertOne({
+        name: 'gabrielp',
+        email: 'gabriel.prij9@hotmail.com',
+        password: '123',
+        role: 'admin'
+      })
+
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+
+      await accountCollection.updateOne({ 
+        _id: id
+      }, {
+         $set: {
+           accessToken
+         }
+      })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{
+            answer: 'any_answer',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'any_answer2'
+          }]
+        })
+        .expect(204)
     })
   })
 })
