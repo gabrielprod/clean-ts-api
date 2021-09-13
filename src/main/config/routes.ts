0039554e7b9784626f87cae9c1ed39271323906d
gabrielpr <gabriel.pr07@hotmail.com>
2021-09-13 14:14:50 -0300

    refactor: allows api to run locally or with docker

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,7 @@ export default (app: Express): void => {
   app.use('/api', router)
   // fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
   readdirSync(path.join(__dirname, '/../routes')).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
