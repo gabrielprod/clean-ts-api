0a64ee6d229f2ebcee0f18ddd516a2c86c8d453e
gabrielpr <gabriel.pr07@hotmail.com>
2021-09-27 22:09:44 -0300

    docs: add login api docs

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,7 +5,6 @@ import { readdirSync } from 'fs'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  // fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
   readdirSync(path.join(__dirname, '/../routes')).map(async file => {
     if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,26 @@
+import {} from './'
+import { loginPath } from './paths/login-paths'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso de clean-architecture para realizar enquetes entre programadores',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
