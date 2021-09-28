024d76083fc8875108c33611df896667babd8c7d
gabrielpr <gabriel.pr07@hotmail.com>
2021-09-28 14:22:38 -0300

    docs: add load surveys api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,12 +1,17 @@
-import {} from './'
 import { badRequest } from './components/bad-request'
+import { forbidden } from './components/forbidden'
 import { notFound } from './components/not-found'
 import { serverError } from './components/server-error'
 import { unauthorized } from './components/unauthorized'
 import { loginPath } from './paths/login-paths'
+import { surveyPath } from './paths/surveys-path'
 import { accountSchema } from './schemas/account-schema'
+import { apiKeyAuthSchema } from './schemas/api-key-schema'
 import { errorSchema } from './schemas/error-schema'
 import { loginParamsSchema } from './schemas/login-params-schema'
+import { surveyAnswerSchema } from './schemas/survey-answer-schema'
+import { surveySchema } from './schemas/survey-schema'
+import { surveysSchema } from './schemas/surveys-schema'
 
 export default {
   openapi: '3.0.0',
@@ -24,19 +29,29 @@ export default {
   }],
   tags: [{
     name: 'Login'
+  }, {
+    name: 'Enquete'
   }],
   paths: {
-    '/login': loginPath
+    '/login': loginPath,
+    '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
-    error: errorSchema
+    error: errorSchema,
+    surveys: surveysSchema,
+    survey: surveySchema,
+    surveyAnswer: surveyAnswerSchema
   },
   components: {
+    securitySchemes: {
+      apiKeyAuth: apiKeyAuthSchema
+    },
     badRequest,
     serverError,
     unauthorized,
-    notFound
+    notFound,
+    forbidden
   }
 }
