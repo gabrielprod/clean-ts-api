5c9ef0884eb67a44522e7fec8b2cb300d9b7b688
gabrielpr <gabriel.pr07@hotmail.com>
2021-09-28 22:54:31 -0300

    docs: add add-signup api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -4,11 +4,14 @@ import { notFound } from './components/not-found'
 import { serverError } from './components/server-error'
 import { unauthorized } from './components/unauthorized'
 import { loginPath } from './paths/login-paths'
+import { signUpPath } from './paths/signup-path'
 import { surveyPath } from './paths/surveys-path'
 import { accountSchema } from './schemas/account-schema'
+import { addSurveyParamsSchema } from './schemas/add-survey-params-schema'
 import { apiKeyAuthSchema } from './schemas/api-key-schema'
 import { errorSchema } from './schemas/error-schema'
 import { loginParamsSchema } from './schemas/login-params-schema'
+import { signUpParamsSchema } from './schemas/signup-params-schema'
 import { surveyAnswerSchema } from './schemas/survey-answer-schema'
 import { surveySchema } from './schemas/survey-schema'
 import { surveysSchema } from './schemas/surveys-schema'
@@ -34,15 +37,18 @@ export default {
   }],
   paths: {
     '/login': loginPath,
+    '/signup': signUpPath,
     '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
+    signUpParams: signUpParamsSchema,
     loginParams: loginParamsSchema,
     error: errorSchema,
     surveys: surveysSchema,
     survey: surveySchema,
-    surveyAnswer: surveyAnswerSchema
+    surveyAnswer: surveyAnswerSchema,
+    addSurveyParams: addSurveyParamsSchema
   },
   components: {
     securitySchemes: {
